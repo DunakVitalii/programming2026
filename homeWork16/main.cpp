73b84f9e897c422d63b4127a33f1955cfbf0b692
Vitalii <mari.dunak@gmail.com>
2026-02-19 21:03:47 -0200

    initial Commit

---
+++ homeWork16/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int f;
+    f = 50;
+    f = 30 + 40;
+    cout << f << endl;
+
+    double debXEnter = 14.0 / 58;
+    double part2 = cos(debXEnter);
+    cout << "Resultat 14/58 =" << part2 << endl;
+
+    double dodepY = 8.0 / 70;
+    double part3 = cos(dodepY);
+    cout << "Resultat 8/70 =" << part3 << endl;
+
+    double privZ = 12.0 / 47;
+    double part4 = sin(privZ);
+    cout << "Resultat 12/47 =" << part4 << endl;
+
+    double exem1 = 80 - 47 /0.2 + 39;
+    double part5 =cos(exem1);
+    cout << "Resultat exem1 =" << exem1 << endl;
+
+    double buket = 50.0;
+    double orange = 30.0;
+    cout << "Orange in buket =" << orange << endl;
+
+    cout << "reed gif" << endl;
+
+    double gif;
+    cin >> gif;
+
+
+    return 0;
+}
