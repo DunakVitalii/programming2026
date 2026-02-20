69e4350fdadfff4437f9c7586c4fdc18a2e004cd
Vitalii <mari.dunak@gmail.com>
2026-02-20 00:38:10 -0200

    codeFIX

--- homeWork16/main.cpp
+++ homeWork16/main.cpp
@@ -6,42 +6,6 @@ using namespace std;
 
 int main(){
 
-
-    cout << "(char)219 = " << (char)219 << endl; // ■ повний квадрат
-    cout << "(char)254 = " << (char)254 << endl; // ■ маленький квадрат
-    cout << "(char)177 = " << (char)177 << endl; // ▒ середній блок
-    cout << "(char)178 = " << (char)178 << endl; // ▓ темний блок
-    cout << "(char)176 = " << (char)176 << endl; // ░ світлий блок
-    cout << "(char)220 = " << (char)220 << endl; // ▄ нижній блок
-    cout << "(char)223 = " << (char)223 << endl; // ▀ верхній блок
-
-    int f;
-    f = 50;
-    f = 30 + 40;
-    cout << f << endl;
-
-    double debXEnter = 14.0 / 58;
-    double part2 = cos(debXEnter);
-    cout << "Resultat 14/58 =" << part2 << endl;
-
-    double dodepY = 8.0 / 70;
-    double part3 = cos(dodepY);
-    cout << "Resultat 8/70 =" << part3 << endl;
-
-    double privZ = 12.0 / 47;
-    double part4 = sin(privZ);
-    cout << "Resultat 12/47 =" << part4 << endl;
-
-    double exem1 = 80 - 47 /0.2 + 39;
-    double part5 =cos(exem1);
-    cout << "Resultat exem1 =" << exem1 << endl;
-
-    double buket = 50.0;
-    double orange = 30.0;
-    cout << "Orange in buket =" << orange << endl;
-
-    cout << "reed gif" << endl;
-
     cout << "Enter Min Limit of Thermometer " ;
     double minLimit = 0.0;
     cin >> minLimit;
@@ -63,7 +27,7 @@ int main(){
    while (!stop) {
     
         myThermometer.showThermometer();    
-        cout << "Enter value to increase temperature (or a non-numeric value to stop): ";
+        cout << "Enter command (1 to increase temperature, 2 to decrease temperature, or a non-numeric value to stop): ";
         int command;
         if (cin >> command) {
             if (command == 1) {
