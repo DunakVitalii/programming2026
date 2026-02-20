2622eb737c4ef34cb0cc88679b656bf0272d973a
Vitalii <mari.dunak@gmail.com>
2026-02-20 00:32:25 -0200

    HomeWork16: thermometer contructor, increase and decrease methods implemented

--- homeWork16/main.cpp
+++ homeWork16/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include "thermometer/Thermometer.h"
 using namespace std;
 
 int main(){
+
+
+    cout << "(char)219 = " << (char)219 << endl; // ■ повний квадрат
+    cout << "(char)254 = " << (char)254 << endl; // ■ маленький квадрат
+    cout << "(char)177 = " << (char)177 << endl; // ▒ середній блок
+    cout << "(char)178 = " << (char)178 << endl; // ▓ темний блок
+    cout << "(char)176 = " << (char)176 << endl; // ░ світлий блок
+    cout << "(char)220 = " << (char)220 << endl; // ▄ нижній блок
+    cout << "(char)223 = " << (char)223 << endl; // ▀ верхній блок
+
     int f;
     f = 50;
     f = 30 + 40;
@@ -31,9 +42,56 @@ int main(){
 
     cout << "reed gif" << endl;
 
-    double gif;
-    cin >> gif;
+    cout << "Enter Min Limit of Thermometer " ;
+    double minLimit = 0.0;
+    cin >> minLimit;
+    cout << "You entered: " << minLimit << endl;
 
+     cout << "Enter Max Limit of Thermometer " ;
+    double maxLimit = 0.0;
+    cin >> maxLimit;
+    cout << "You entered: " << maxLimit << endl;
 
+    cout << "Enter Current Temperature of Thermometer " ;
+    double curTemperature = 0.0;
+    cin >> curTemperature;
+   cout << "You entered: " << curTemperature << endl;
+
+   Thermometer myThermometer(curTemperature, minLimit, maxLimit);
+
+   bool stop = false;
+   while (!stop) {
+    
+        myThermometer.showThermometer();    
+        cout << "Enter value to increase temperature (or a non-numeric value to stop): ";
+        int command;
+        if (cin >> command) {
+            if (command == 1) {
+                cout << "Enter value to increase temperature: ";
+                double increaseValue;
+                if (cin >> increaseValue) {
+                    cout << "Current Temperature after increase: " << myThermometer.increaseTemperature(increaseValue) << endl;
+                } else {
+                    stop = true;
+                    cout << "Stopping temperature increase." << endl;
+                }
+            } else if (command == 2) {
+                cout << "Enter value to decrease temperature: ";
+                double decreaseValue;
+                if (cin >> decreaseValue) {
+                    cout << "Current Temperature after decrease: " << myThermometer.decreaseTemperature(decreaseValue) << endl;
+                } else {
+                    stop = true;
+                    cout << "Stopping temperature decrease." << endl;
+                }
+            } else {
+                cout << "Invalid command. Please enter 1 to increase temperature, 2 to decrease temperature, or a non-numeric value to stop." << endl;
+            }
+        } else {
+            stop = true;
+            cout << "Stopping temperature increase." << endl;
+        }        
+    }
+    
     return 0;
 }
