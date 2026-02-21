d9f94ea8d559248afdd68598eb91e09401cad1a1
Vitalii <mari.dunak@gmail.com>
2026-02-21 18:25:57 -0200

    Functions implementation moved to cpp file

--- homeWork16/thermometer/Thermometer.h
+++ homeWork16/thermometer/Thermometer.h
@@ -13,52 +13,9 @@ private:
 
 public:
 
-    Thermometer(double cTemp, double lMin, double lMax)
-    {
-        currentTemperature = cTemp;
-        minLimit = lMin;
-        maxLimit = lMax;
-    }
-    
-    void showThermometer()
-    {
-        cout << "Minimum Limit: " << minLimit << "C |"; 
-        for(double i = minLimit; i < maxLimit; i+=0.1) {
-            double precision = 10.0; // Number of decimal places to round to
-            double roundedIndex = round(i * precision) / precision; // Round to one decimal place
-            double roundedCurrentTemp = round(currentTemperature * precision) / precision; // Round to one decimal place
-            if (roundedIndex == roundedCurrentTemp) {
-                cout << " " << roundedIndex << "C ";
-            } else {
-                if (roundedIndex > roundedCurrentTemp) {
-                    cout << "-";
-                } else {
-                    cout << (char)177;  // ASCII character for a filled block
-                }
-            }
-        }
-       cout << "| Maximum Limit: " << maxLimit << "C" << endl;
-       cout << "Current Temperature: " << currentTemperature << endl;
-    }
-    
-    double increaseTemperature(double newValue)
-    {
-        if ((currentTemperature + newValue) > maxLimit) {
-            cout << "Maximum Limit Reached! Temperature cannot exceed " << maxLimit << "C" << endl;
-        } else {
-            currentTemperature += newValue;
-        }
-        return currentTemperature;
-    }
-   
-
-    double decreaseTemperature(double newValue) {
-        if ((currentTemperature - newValue) < minLimit) {
-            cout << "Minimum Limit Reached! Temperature cannot be less than " << minLimit << "C" << endl;
-        } else {
-            currentTemperature -= newValue;
-        }
-        return currentTemperature;
-    }
-
+    Thermometer(double cTemp, double lMin, double lMax);
+        
+    void showThermometer();
+    double increaseTemperature(double newValue);
+    double decreaseTemperature(double newValue);
 };
